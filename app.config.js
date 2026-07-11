d4ad64ece6c8b630241f9e33f0fa1d84dc326741
Manniegenie <Manuelwurld@gmail.com>
2026-07-11 11:47:31 +0100

    bump iOS buildNumber to 9

--- app.config.js
+++ app.config.js
@@ -16,7 +16,7 @@ export default {
     ios: {
       supportsTablet: true,
       bundleIdentifier: IS_STAGING ? "com.manniegenie.zeusodx.staging" : "com.manniegenie.zeusodx",
-      buildNumber: "7",
+      buildNumber: "9",
       icon: "./assets/images/app-icon.png",
       infoPlist: {
         ITSAppUsesNonExemptEncryption: false,
